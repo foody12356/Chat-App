835134dc392d3489c986036fe94dd625c2fb7c84
foody12356 <98463357+foody12356@users.noreply.github.com>
2023-07-31 22:54:59 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import ChatProvider from "./Context/ChatProvider";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <ChatProvider>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </ChatProvider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
