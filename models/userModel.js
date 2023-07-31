ab85b81772d934655fa464ebd3e10c011a3c3983
foody12356 <98463357+foody12356@users.noreply.github.com>
2023-07-31 22:53:47 +0530

    Add files via upload

---
+++ models/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    pic: {
+      type: String,
+      default:
+        "https://1fid.com/wp-content/uploads/2022/06/no-profile-picture-6-1024x1024.jpg",
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+//for encoding the password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
